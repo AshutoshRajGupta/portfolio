fd6ef468b77df33e87852e1909c8b32b73663102
Ashutosh Raj Gupta <ag2364443@gmail.com>
2023-02-03 04:11:04 +0530

    updated frontend

---
+++ personal-portfolio/src/components/ProjectCard.js
@@ -0,0 +1,18 @@
+import { Col } from "react-bootstrap";
+
+export const ProjectCard = ({ title, description, imgUrl }) => {
+  return (
+    <Col size={12} sm={6} md={4}>
+      <div className="proj-imgbx">
+        <img src={imgUrl} />
+        <div className="proj-txtx">
+        <h4>{title}</h4>
+
+        <a href="https://www.w3schools.com" target='_blank'>
+          <span>{description}</span>
+          </a>
+        </div>
+      </div>
+    </Col>
+  )
+}
